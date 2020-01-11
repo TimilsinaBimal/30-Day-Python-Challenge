7be904d407e00ce56e9157686e17a24cef33e037
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-01-11 23:49:46 +0545

    Day 10

---
+++ day10.py
@@ -0,0 +1,11 @@
+# SalesPerson Rita Drives 2052 miles in 6 days
+# stopping at 2 towns each day. How many Kilometers
+# does she average between stops?
+
+totalDistanceTravelled = 2052
+totalDaysTravelled = 6
+totalStoppedEachDay = 2
+mileToKm = 1.60934
+averageStopped = totalDistanceTravelled / (totalDaysTravelled * totalStoppedEachDay)
+averageStoppedInKm = averageStopped * mileToKm
+print(f"Average between Stops : {averageStoppedInKm} ")
