9de4f9edb66c96fada904f920a9a6481f348b7d0
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-01-23 11:46:22 +0545

    Day 11

---
+++ day11.py
@@ -0,0 +1,9 @@
+# Dan Rented two movies to watch last night.
+# The first was 100 minutes long, the second 110 minutes long.
+# How many hours did it take for Dan to watch the two movies
+firstMovieDurationInMinutes = 100
+secondMovieDurationInMinutes = 110
+minutesToHour = 60
+totalDuration = (float)(firstMovieDurationInMinutes +
+                        secondMovieDurationInMinutes)/minutesToHour
+print(f"Dan Took {totalDuration} hours to watch two movies")
