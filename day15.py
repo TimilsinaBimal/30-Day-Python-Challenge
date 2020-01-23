399512cf4db5375e4ed8dc06c407cf9554e7ea08
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-01-23 22:38:50 +0545

    Day 15

---
+++ day15.py
@@ -0,0 +1,9 @@
+# Lucille spent 12% of her weekly earnings on
+# DVDs and deposited the rest into her savings
+# account. If she spent $42 on DVDs, how much
+# did she deposit into her savings Account?
+
+percentSpent = 12
+rupeesSpent = 42
+amountDeposited = (rupeesSpent/percentSpent)*100 - rupeesSpent
+print(f"Amount Deposited: {amountDeposited}")
