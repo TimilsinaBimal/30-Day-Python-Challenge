3214b6a69347a398374809ff5b88356182d275ba
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-01-23 23:39:30 +0545

    Day 18

---
+++ day18.py
@@ -0,0 +1,8 @@
+# Lefty keeps track of the length of eac fish that he catches
+# Below are the lengths in inches of the fish that he caught one day:
+# 12, 13, 8, 10, 17
+# What is the largest fish length that lefty caught that day?
+# Solve without using conditional statements and the loop.
+
+lengthOfFish = [12, 13, 8, 10, 17]
+print(f"The max length of fish is {max(lengthOfFish)}")
