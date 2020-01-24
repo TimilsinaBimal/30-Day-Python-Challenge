b0783efe731fac384baa8e146092566cad7c7f41
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-01-24 10:59:22 +0545

    Day 14

--- day14.py
+++ day14.py
@@ -1,3 +1,24 @@
 # How many ways can four students Ram, Anuj, Deepak and Ravi line up in
 # a line, if the order matters?
 # Print all the possible Combination.
+
+
+def all_combination(arr):
+    if len(arr) == 0:
+        return []
+
+    if len(arr) == 1:
+        return [arr]
+
+    comb = []
+    for i in range(len(arr)):
+        temp = arr[i]
+        remaining = arr[:i] + arr[i+1:]
+        for a in all_combination(remaining):
+            comb.append([temp] + a)
+    return comb
+
+
+students = ["Ram", "Anuj", "Deepak", "Ravi"]
+for combination in all_combination(students):
+    print(combination)
